6bdc353935d584b251a599617f28203f68b598ce
teifangu <ifaneds1@gmail.com>
2025-06-12 17:55:04 +0100

    FullPhysics

---
+++ Source/RoomOne/Public/MyRoomOneGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RoomOneGameMode.h"
+#include "MyRoomOneGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMONE_API AMyRoomOneGameMode : public ARoomOneGameMode
+{
+	GENERATED_BODY()
+	
+};
